c864f06d24c6b6a9d920b20f04f256fbb4817c97
pradeepmalvi <p.malviyaaa@gmail.com>
2023-03-06 17:03:51 +0530

    Ui updates

--- frontend_react/src/container/Work/Work.tsx
+++ frontend_react/src/container/Work/Work.tsx
@@ -62,19 +62,17 @@ const Work: FC = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["UI/UX", "Web App", "Mobile App", "All"].map(
-          (item, index) => (
-            <div
-              key={index}
-              onClick={() => handleWorkFilter(item)}
-              className={`app__work-filter-item app__flex p-text ${
-                activeFilter === item ? "item-active" : ""
-              }`}
-            >
-              {item}
-            </div>
-          )
-        )}
+        {["Web App", "Mobile App", "All"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
       </div>
 
       <motion.div
--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -7,11 +7,7 @@ const AppWrap = (Component, idName, classNames) =>
       <div id={idName} className={`app__container ${classNames}`}>
         <SocialMedia />
         <div className="app__wrapper app__flex">
-          <Component />
-
-          <div className="copyright"> 
-            <p className="p-text">@2023 | All rights reserved</p>
-          </div>
+          <Component /> 
         </div>
         <NavigationDots active={idName} />
       </div>
